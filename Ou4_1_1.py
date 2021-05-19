48313a801ec6afe45bb7322ed4dbf166ff9b1056
Hampus Naumanen <hampus.naumanen.94@hotmail.com>
2021-05-19 21:41:52 +0200

    Plotting red points inside, and blue points outside

--- Ou4_1_1.py
+++ Ou4_1_1.py
@@ -8,28 +8,44 @@ Created on Wed May 19 19:52:15 2021
 """ Program that estimates the value of PI by using Monte Carlo method"""
 
 import random
+import math
+import matplotlib.pyplot as plt
 
 
 def estimate_PI_MC(n):
     
-    points_in_circle = 0
+    no_points_in_circle = 0
+    x_points_inside = []
+    y_points_inside = []
+    x_points_outside = []
+    y_points_outside = []
     for i in range(n):
         
         rand_x = random.uniform(-1, 1)
-        rand_y = random.uniform(-1, 1)        
+        rand_y = random.uniform(-1, 1)
         
         if rand_x**2 + rand_y**2 <= 1:
-            points_in_circle += 1
+            no_points_in_circle += 1
+            x_points_inside.append(rand_x)
+            y_points_inside.append(rand_y)
+        else:
+            x_points_outside.append(rand_x)
+            y_points_outside.append(rand_y)
         
-    print(f'Number of points inside the circle: {points_in_circle}')
+    print(f'Number of points inside the circle: {no_points_in_circle}')
     
-    pi_estimate = 4*points_in_circle/n
-    print(f'Estimate of pi with {n} points: {pi_estimate}')
+    pi_estimate = 4*no_points_in_circle/n
+    print(f'Estimate of PI with {n} points: {pi_estimate}')
+    print(f'"Real" vale of PI: {math.pi}')
+    
+    plt.plot(x_points_inside, y_points_inside, 'ro')
+    plt.plot(x_points_outside, y_points_outside, 'bo')
+    plt.axis([-1,1,-1,1])
     
     
 
 def main():
-    estimate_PI_MC(1000)
+    estimate_PI_MC(10)
     
 
 if __name__ == "__main__":
