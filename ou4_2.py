8da3c3c560c70356f0eceaf49abd59d82ccd0d1d
Hampus Naumanen <hana7627@arrhenius.it.uu.se>
2021-06-01 19:39:44 +0200

    Fib method working for pythonfile

--- ou4_2.py
+++ ou4_2.py
@@ -9,7 +9,10 @@ def main():
 	f.set(7)
 	print(f.get())
 	print()
-	fib10 =  fib_py(Heltal(10))
+	#fib10 =  fib_py(Heltal(10))
+	#print(fib10)
+	print("Testing fibonacci method: computing 10th value")
+	fib10 = fib_py(10)
 	print(fib10)
 
 
