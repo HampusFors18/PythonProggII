9c55b4aa9e1d1a75f650d44d1ef519297d1ccad4
Hampus Naumanen <hampus.naumanen.94@hotmail.com>
2021-06-02 20:03:30 +0200

    Changed some ctypes in heltal.py

--- heltal.py
+++ heltal.py
@@ -3,23 +3,28 @@ import ctypes
 lib = ctypes.cdll.LoadLibrary('./libheltal.so')
 
 class Heltal(object):
-	def __init__(self, val):
-		lib.Heltal_new.argtypes = [ctypes.c_int]
-		lib.Heltal_new.restype = ctypes.c_void_p
-		lib.Heltal_get.argtypes = [ctypes.c_void_p]
-		lib.Heltal_get.restype = ctypes.c_int
-		lib.Heltal_set.argtypes = [ctypes.c_void_p,ctypes.c_int]
-		lib.Heltal_delete.argtypes = [ctypes.c_void_p]
-		self.obj = lib.Heltal_new(val)
+    def __init__(self, val):
+        lib.Heltal_new.argtypes = [ctypes.c_int]
+        lib.Heltal_new.restype = ctypes.c_void_p
+        lib.Heltal_get.argtypes = [ctypes.c_void_p]
+        lib.Heltal_get.restype = ctypes.c_int
+        lib.Heltal_set.argtypes = [ctypes.c_void_p,ctypes.c_int]
+        lib.Heltal_fib.argtypes = [ctypes.c_void_p]
+        lib.Heltal_fib.restypes = ctypes.c_int
+        lib.Heltal_delete.argtypes = [ctypes.c_void_p]
+        self.obj = lib.Heltal_new(val)
 
-	def get(self):
-		return lib.Heltal_get(self.obj)
+    def get(self):
+        return lib.Heltal_get(self.obj)
 
-	def set(self, val):
-		lib.Heltal_set(self.obj, val)
+    def set(self, val):
+        lib.Heltal_set(self.obj, val)
         
-	def __del__(self):
-		return lib.Heltal_delete(self.obj)
+    def fib(self):
+        return lib.Heltal_fib(self.obj)
+        
+    def __del__(self):
+        return lib.Heltal_delete(self.obj)
 	
 def fib_py(n):
 	if n <= 1:
